98e516b084ea662eaaa3d7b95778400e57379e5e
jonowood <113280744+jonowood@users.noreply.github.com>
2022-12-09 15:00:20 +0800

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,7 +8,7 @@ csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 
 
 
-#######print("HELLO")
+print("HELLO")
 
 # In this challenge, you are tasked with creating a Python script to analyse the financial records of your company. You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: "Date" and "Profit/Losses". (Thankfully, your company has rather lax standards for accounting, so the records are simple.)
 
