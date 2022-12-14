eb04a77e25255bbc16714a724cff476963262755
jonowood <113280744+jonowood@users.noreply.github.com>
2022-12-14 16:17:59 +0800

    I should of done this weeks ago now
     ive forgotten commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,7 @@
 # import OS and CSV module
 import os
 import csv
+from collections import Counter
 
 # set path for budget_data.csv
 csvpath = os.path.join('Resources', 'election_data.csv')
@@ -15,40 +16,36 @@ with open(csvpath) as csvfile:
 # create lists to store data, loop to capture
     votes = []
     candidates = []
-    candidate_votes = 0
     unique_candidates = list(set(candidates))
 
     for v in csv_reader:
         votes.append(v[0])
         candidates.append(v[2])
 
+candidate_count = Counter(candidates)
+
+total_votes = len(votes)
+print(total_votes)
+print(unique_candidates)
+print(candidate_count)
 
 
+# split dictionary into keys and values
+keys = []
+values = []
+for i in candidate_count:
+    keys.append(i)
+    values.append(candidate_count[i])
 
-
+print("keys : ", str(keys))
+print("values : ", str(values))
 
 # A complete list of candidates who received votes
 
-
-
-# up to here ##################################  
-
-
-
- 
 # The total number of votes cast
-total_votes = len(votes)
-candidate_count = {candidate:candidates.count(candidate) for candidate in candidates}
-
-print(total_votes)
-print(candidate_count[1])
-
 # The percentage of votes each candidate won
-
-
 # PyPoll Instructions
 # In this Challenge, you are tasked with helping a small, rural U.S. town modernise its vote-counting process.
-
 # three columns: "Voter ID", "County", and "Candidate"
 
 
